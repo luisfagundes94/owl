0eebb8cb52725f7be4e9aca8df7d368d487001d3
Luis Felipe Fagundes <luisfelipecf94@gmail.com>
2025-05-31 13:28:42 -0300

    update DeviceListScreen to use primary color for IP address text

--- feature/device/src/main/java/com/luisfagundes/device/presentation/list/DeviceListScreen.kt
+++ feature/device/src/main/java/com/luisfagundes/device/presentation/list/DeviceListScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -109,7 +110,8 @@ internal fun DeviceCard(
             )
             Text(
                 text = device.ipAddress,
-                style = MaterialTheme.typography.labelMedium
+                style = MaterialTheme.typography.labelMedium,
+                color = MaterialTheme.colorScheme.primary
             )
         }
     }

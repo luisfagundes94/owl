88e3ec8b4fbd21c69e606e63f8c7a81162de4fc8
Luis Felipe Fagundes <luisfelipecf94@gmail.com>
2025-05-31 17:14:25 -0300

    renaming AndroidFrameworkModule to ConnectivityModule

--- core/network/src/main/java/com/luisfagundes/network/di/ConnectivityModule.kt
+++ core/network/src/main/java/com/luisfagundes/network/di/ConnectivityModule.kt
@@ -11,7 +11,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object AndroidFrameworkModule {
+object ConnectivityModule {
     @Provides
     @Singleton
     fun provideConnectivityManager(

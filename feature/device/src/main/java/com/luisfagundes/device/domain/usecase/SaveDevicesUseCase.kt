75cedc85df141006d3c6e93e222cc1e1c1188ce3
Luis Felipe Fagundes <luisfelipecf94@gmail.com>
2025-06-01 19:00:39 -0300

    enhance SaveDevicesUseCase to manage device states and save new device states

--- feature/device/src/main/java/com/luisfagundes/device/domain/usecase/SaveDevicesUseCase.kt
+++ feature/device/src/main/java/com/luisfagundes/device/domain/usecase/SaveDevicesUseCase.kt
@@ -2,10 +2,24 @@ package com.luisfagundes.device.domain.usecase
 
 import com.luisfagundes.domain.model.Device
 import com.luisfagundes.domain.repository.HistoryRepository
+import kotlinx.coroutines.flow.firstOrNull
 import javax.inject.Inject
 
 internal class SaveDevicesUseCase @Inject constructor(
     private val historyRepository: HistoryRepository
 ) {
-    suspend operator fun invoke(devices: List<Device>) = historyRepository.saveDevices(devices)
+    suspend operator fun invoke(scannedDevices: List<Device>) {
+        val previouslySavedDeviceList = historyRepository.getDevices().firstOrNull() ?: emptyList()
+        val scannedDeviceMap = scannedDevices.associateBy { it.ipAddress }
+        val previouslySavedDeviceMap = previouslySavedDeviceList.associateBy { it.ipAddress }
+        val allKnownIps = (scannedDeviceMap.keys + previouslySavedDeviceMap.keys)
+
+        val newDeviceStatesToSave = allKnownIps.mapNotNull { ip ->
+            val deviceFromScan = scannedDeviceMap[ip]
+            val deviceFromCache = previouslySavedDeviceMap[ip]?.copy(isActive = false)
+            deviceFromScan ?: deviceFromCache
+        }
+
+        historyRepository.saveDevices(newDeviceStatesToSave)
+    }
 }

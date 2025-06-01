916e254a7b9fda72f85291cee6381037d5b048f0
Luis Felipe Fagundes <luisfelipecf94@gmail.com>
2025-06-01 18:42:24 -0300

    feat: update insertDevices method to handle conflicts using REPLACE strategy

--- feature/history/src/main/java/com/luisfagundes/history/data/dao/DeviceDao.kt
+++ feature/history/src/main/java/com/luisfagundes/history/data/dao/DeviceDao.kt
@@ -2,13 +2,14 @@ package com.luisfagundes.history.data.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.luisfagundes.history.data.model.DeviceEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 internal interface DeviceDao {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertDevices(deviceEntityList: List<DeviceEntity>)
 
     @Query("SELECT * FROM device ORDER BY timestamp DESC")

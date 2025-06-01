c3e1c977935b590515b68a5369beda89c5b5ccc4
Luis Felipe Fagundes <luisfelipecf94@gmail.com>
2025-06-01 11:26:23 -0300

    refactor: remove unused import from DeviceListScreen

--- feature/device/src/main/java/com/luisfagundes/device/presentation/list/DeviceListScreen.kt
+++ feature/device/src/main/java/com/luisfagundes/device/presentation/list/DeviceListScreen.kt
@@ -18,7 +18,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.core.graphics.BlendModeColorFilterCompat
 import androidx.core.graphics.BlendModeCompat
 import androidx.hilt.navigation.compose.hiltViewModel

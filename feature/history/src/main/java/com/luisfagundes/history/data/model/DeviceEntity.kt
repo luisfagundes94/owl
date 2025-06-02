34e0be4da4cf0a0b4e89b1d2070d99dd94b8e3c0
Luis Felipe Fagundes <luisfelipecf94@gmail.com>
2025-06-02 20:25:29 -0300

    fix: change primary key in DeviceEntity to use ipAddress instead of auto-generated id

--- feature/history/src/main/java/com/luisfagundes/history/data/model/DeviceEntity.kt
+++ feature/history/src/main/java/com/luisfagundes/history/data/model/DeviceEntity.kt
@@ -5,8 +5,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "device")
 data class DeviceEntity(
-    @PrimaryKey(autoGenerate = true) val id: Long = 0,
-    val ipAddress: String,
+    @PrimaryKey val ipAddress: String,
     val hostName: String,
     val isActive: Boolean,
     val timestamp: Long
